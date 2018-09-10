b19b10ec6c10a3c6cbce4a3304b027a25c4e5985
David Guyo <David_Guyo@epam.com>
2018-09-10 10:15:22 +0300

    Step 1

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -4,6 +4,7 @@ package com.andreitop.newco.aspect;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,13 @@ public class LoggingAspect {
     public void beforeRepoFind(JoinPoint joinPoint) {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
-        logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called Before");
     }
 
+    @After("com.andreitop.newco.aspect.PointcutContainer.repositoryFind()")
+    public void afterRepoFind(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called After");
+    }
 }
